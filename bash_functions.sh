fc8d557ba42325e72c6b00ab435ef82d0796925b
Daniele Monti <62102073+Monska85@users.noreply.github.com>
2024-08-26 17:58:19 +0200

    feat: add bash function to print the basic auth informations

--- bash_functions.sh
+++ bash_functions.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
 kga() {
   NAMESPACE="${1}"
@@ -12,4 +12,29 @@ kga() {
     echo -e "Show all resources in \e[1m\e[32m${NAMESPACE}\e[39m\e[0m namespace"
     kubectl api-resources --verbs=list --namespaced -o name | xargs -n 1 kubectl get --show-kind --ignore-not-found -n "${NAMESPACE}"
   fi
+}
+
+function print-basic-auth() {
+  local CURRENT_NAMESPACE INGRESS FIRST_HOST SECRET USERNAME PASSWORD
+
+  CURRENT_NAMESPACE="${1:-"$(kubectl config view --minify -o jsonpath="{..namespace}")"}"
+  echo "Discovering configured ingresses in namespace: ${CURRENT_NAMESPACE}"
+
+  for INGRESS in $(kubectl --namespace "${CURRENT_NAMESPACE}" get ingresses -o jsonpath='{.items[*].metadata.name}'); do
+    FIRST_HOST=$(kubectl --namespace "${CURRENT_NAMESPACE}" get ingress "${INGRESS}" -o jsonpath="{.spec.rules[0].host}")
+    SECRET=$(kubectl --namespace "${CURRENT_NAMESPACE}" get ingress "${INGRESS}" -o jsonpath="{.metadata.annotations.nginx\\.ingress\\.kubernetes\\.io/auth-secret}")
+    if [ -z "${SECRET}" ]; then
+      echo "No auth secret found for ingress ${INGRESS} (${FIRST_HOST})"
+      continue
+    fi
+    USERNAME=$(kubectl --namespace "${CURRENT_NAMESPACE}" get secret "${SECRET}" -o jsonpath="{.data.username}" | base64 --decode)
+    PASSWORD=$(kubectl --namespace "${CURRENT_NAMESPACE}" get secret "${SECRET}" -o jsonpath="{.data.password}" | base64 --decode)
+    if [ -z "${USERNAME}" ] || [ -z "${PASSWORD}" ]; then
+      echo "No auth credentials found in secret ${SECRET} (${INGRESS} - ${FIRST_HOST})"
+      continue
+    fi
+    echo "Auth credentials for ingress ${INGRESS} (${FIRST_HOST}): ${USERNAME} / ${PASSWORD}"
+  done
+
+  echo "Done"
 }
